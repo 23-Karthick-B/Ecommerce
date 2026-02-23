b1567a2c477f3b6cefc138b637b73279ae3fab84
Karthick B <officialkarthick23@gmail.com>
2026-02-23 18:42:05 +0530

    Controller logic for view products

--- src/main/java/com/project/ecommerce/controller/ProductController.java
+++ src/main/java/com/project/ecommerce/controller/ProductController.java
@@ -1,10 +1,26 @@
 package com.project.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.ecommerce.entity.Product;
+import com.project.ecommerce.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
 }
--- src/main/java/com/project/ecommerce/service/ProductService.java
+++ src/main/java/com/project/ecommerce/service/ProductService.java
@@ -2,6 +2,7 @@ package com.project.ecommerce.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.ecommerce.entity.Product;
@@ -9,9 +10,9 @@ import com.project.ecommerce.repository.ProductRepository;
 
 @Service
 public class ProductService {
-
+    @Autowired
     private ProductRepository productRepository;
-
+    
     public List<Product> getAllProducts(){
         List<Product> products = productRepository.findAll();
         return products;
