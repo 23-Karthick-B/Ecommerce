3a6860162208b987393c7581e0f675fc4f874c3c
Karthick B <officialkarthick23@gmail.com>
2026-02-23 21:51:32 +0530

    Adding Pagination for Product List

--- src/main/java/com/project/ecommerce/service/ProductService.java
+++ src/main/java/com/project/ecommerce/service/ProductService.java
@@ -1,21 +1,31 @@
 package com.project.ecommerce.service;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.project.ecommerce.entity.Product;
 import com.project.ecommerce.repository.ProductRepository;
 
+
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
-    
-    public List<Product> getAllProducts(){
-        List<Product> products = productRepository.findAll();
-        return products;
+
+    public Map<String,Object> getAllProducts(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> products =productRepository.findAll(pageable);
+
+        Map<String,Object> response = new HashMap<>();
+        response.put("products", products.getContent());
+        response.put("totalProducts", products.getTotalElements());
+        return response;
     }
 
 }
