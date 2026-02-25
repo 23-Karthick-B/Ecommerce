011908d0d706a0483e76a5eee9abd6070c1271ce
Karthick B <officialkarthick23@gmail.com>
2026-02-25 21:46:46 +0530

    Product Price Filter API

--- src/main/java/com/project/ecommerce/spec/ProductSpecification.java
+++ src/main/java/com/project/ecommerce/spec/ProductSpecification.java
@@ -9,7 +9,14 @@ public class ProductSpecification {
         return (root,query,cb) -> category == null ? null : cb.equal(root.get("category"), category);
         // This is a Specification interface where it returns root,query and callback.
         // We are checking whether the category exists if null return null, if exists then get the query.
-
+    }
+    public static Specification<Product> priceBetween(Double min, Double max){
+        return (root,query,cb) -> {
+            if(min == null && max == null) return null;
+            if(min == null) return cb.lessThanOrEqualTo(root.get("price"), max);
+            if(max == null) return cb.greaterThanOrEqualTo(root.get("price"), min);
+            return cb.between(root.get("price"), min, max);
+        };
     }
 
 }
