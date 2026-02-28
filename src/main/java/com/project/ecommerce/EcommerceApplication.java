a87205b09107c878a45b85b977f3f54a3b53a661
Karthick B <officialkarthick23@gmail.com>
2026-02-28 22:08:01 +0530

    Deploy phase

--- src/main/java/com/project/ecommerce/EcommerceApplication.java
+++ src/main/java/com/project/ecommerce/EcommerceApplication.java
@@ -3,9 +3,29 @@ package com.project.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class EcommerceApplication {
+	
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        if (dotenv.get("DB_URL") != null) {
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        }
+        if (dotenv.get("DB_USER") != null) {
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        }
+        if (dotenv.get("DB_PASS") != null) {
+            System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        }
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
